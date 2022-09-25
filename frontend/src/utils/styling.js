99d2815700dc1cd194387e09facc55babbe20173
Sebastian <sebastian.cumins@gmail.com>
2022-09-25 11:26:58 +1000

    Pulled colours from style guide, does not match :(

--- frontend/src/utils/styling.js
+++ frontend/src/utils/styling.js
@@ -1,14 +1,27 @@
 const colours = {
     blue1: "#7BA2D5",
     blue2: "#427BBF",
-    blue1Percent50: "rgb(123, 162, 213, 50%)",
-    blue1Percent80: "rgb(123, 162, 213, 80%)",
-    blue1Percent100: "rgb(123, 162, 213, 100%)",
-    blue2Percent10: "rgb(66, 123, 191, 10%)",
-    blue2Percent30: "rgb(66, 123, 191, 30%)",
-    blue2Percent50: "rgb(66, 123, 191, 50%)",
-    blue2Percent80: "rgb(66, 123, 191, 50%)",
-    blue2Percent100: "rgb(66, 123, 191, 80%)",
+    // Colours pulled from the style guide - Very bad consistency
+    blue1Percent100: "#407bbf",
+    blue1Percent80: "#6e94cd",
+    blue1Percent50: "#9fbddf",
+    blue1Percent30: "#c9d6ec",
+    blue1Percent10: "#ecf2f8",
+    blue2Percent100: "#7aa2d6",
+    blue2Percent80: "#95b5de",
+    blue2Percent50: "#bcd0ea",
+    blue2Percent30: "#d7e3f3",
+    blue2Percent10: "#f1f6fb",
+
+    // Keeping this here incase we want to go back
+    // blue1Percent50: "rgb(123, 162, 213, 50%)",
+    // blue1Percent80: "rgb(123, 162, 213, 80%)",
+    // blue1Percent100: "rgb(123, 162, 213, 100%)",
+    // blue2Percent10: "rgb(66, 123, 191, 10%)",
+    // blue2Percent30: "rgb(66, 123, 191, 30%)",
+    // blue2Percent50: "rgb(66, 123, 191, 50%)",
+    // blue2Percent80: "rgb(66, 123, 191, 50%)",
+    // blue2Percent100: "rgb(66, 123, 191, 80%)",
 };
 
 const fonts = {
