ea91b17c3c8445bd41a2cfe7999c18ce17ee9f94
PikeyAU <66731279+PikeyAU@users.noreply.github.com>
2022-09-26 22:27:52 +1000

    Database connection

--- corelia_project/corelia_project/settings.py
+++ corelia_project/corelia_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'corelia_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coreliaproject',
+        'USER': 'coreliagroup1',
+        'PASSWORD': 'group1corelia',
+        'HOST': 'coreliadb.coyhyjzz186m.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
