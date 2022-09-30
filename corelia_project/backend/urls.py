ffd290ab0a9337152c95b8ed2dfdca6b67dde73d
Sebastian <sebastian.cumins@gmail.com>
2022-09-30 13:34:51 +1000

    fixed backend endpoint urls

--- corelia_project/backend/urls.py
+++ corelia_project/backend/urls.py
@@ -3,14 +3,14 @@ from django.urls import path, include, re_path
 from backend import views
 
 urlpatterns = [
-    path('composer/api', ComposerView.as_view()),
-    path('composition/api', CompositionView.as_view()),
-    path('composerNationality/api', ComposerNationalityView.as_view()),
-    path('compositionInstrument/api', CompositionInstrumentView.as_view()),
-    path('publisher/api', PublisherView.as_view()),
-    path('nationality/api', NationalityView.as_view()),
-    path('instrument/api', InstrumentView.as_view()),
-    re_path(r'^api/featured/$', views.featured_composer),
-    re_path(r'^api/featured/([0-9])$', views.featured_composer_detail),
-    
+    path('composer', ComposerView.as_view()),
+    path('composition', CompositionView.as_view()),
+    path('composerNationality', ComposerNationalityView.as_view()),
+    path('compositionInstrument', CompositionInstrumentView.as_view()),
+    path('publisher', PublisherView.as_view()),
+    path('nationality', NationalityView.as_view()),
+    path('instrument', InstrumentView.as_view()),
+    re_path(r'^featured/$', views.featured_composer),
+    re_path(r'^featured/([0-9])$', views.featured_composer_detail),
+
 ]
