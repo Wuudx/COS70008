85dceea4cf405def2add10adf35dee4b780788c9
Sebastian <sebastian.cumins@gmail.com>
2022-09-30 13:23:19 +1000

    Footer bug fixes

--- corelia_project/frontend/src/components/footer/footer-bottom/FooterBottom.jsx
+++ corelia_project/frontend/src/components/footer/footer-bottom/FooterBottom.jsx
@@ -1,28 +1,29 @@
-import styled from "styled-components";
-import footerLogo from "../../../../static/images/footer-logo.png";
-import stylingConstants from "../../../utils/styling";
-import CopyrightMessage from "./CopyrightMessage";
-import HouseKeepingNav from "./HouseKeepingNav";
-import SocialMediaNav from "./SocialMediaNav";
-import React from "react";
+import styled from 'styled-components';
+import footerLogo from '../../../../static/images/footer-logo.png';
+import stylingConstants from '../../../utils/styling';
+import CopyrightMessage from './CopyrightMessage';
+import SiteNav from './SiteNav';
+import SocialMediaNav from './SocialMediaNav';
+import React from 'react';
 
 const Container = styled.div`
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
     gap: 20px;
     height: 25vh;
     width: 100%;
     background-color: ${stylingConstants.colours.blue1Percent80};
+    padding: 0 30px;
 `;
 
 const FooterBottom = () => {
     return (
         <Container>
             <CopyrightMessage />
-            <HouseKeepingNav />
+            <SiteNav />
             <SocialMediaNav />
-            <img src={footerLogo} alt="Corelia Project Footer Logo" />
+            <img src={footerLogo} alt='Corelia Project Footer Logo' />
         </Container>
     );
 };
--- corelia_project/frontend/src/components/footer/footer-bottom/SiteNav.jsx
+++ corelia_project/frontend/src/components/footer/footer-bottom/SiteNav.jsx
@@ -1,36 +1,35 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import Ul from "../../../shared-styled-components/NavUl";
-import React from "react";
-
-// TODO: THINK FOR A BETTER NAME FOR THIS COMPONENT!!!
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Ul from '../../../shared-styled-components/NavUl';
+import React from 'react';
 
 const StyledLink = styled(Link)`
     font-family: lato-regular;
     text-decoration: none;
     color: white;
+    white-space: nowrap;
 `;
 
-const HouseKeepingNav = () => {
+const SiteNav = () => {
     return (
         <nav>
             <Ul>
                 <li>
-                    <StyledLink to="/terms-of-service/">
+                    <StyledLink to='/terms-of-service/'>
                         Terms of Service |
                     </StyledLink>
                 </li>
                 <li>
-                    <StyledLink to="/privacy-policy/">
+                    <StyledLink to='/privacy-policy/'>
                         &nbsp;Privacy Policy |
                     </StyledLink>
                 </li>
                 <li>
-                    <StyledLink to="/site-map/">&nbsp;Site Map</StyledLink>
+                    <StyledLink to='/site-map/'>&nbsp;Site Map</StyledLink>
                 </li>
             </Ul>
         </nav>
     );
 };
 
-export default HouseKeepingNav;
+export default SiteNav;
