68d410110f90e71c07e24658c4619e4d1578207c
Mitchell Anton <mitchella0100@gmail.com>
2022-09-30 11:53:09 +1000

    Re-add invisible flex item for forum centering.

--- corelia_project/frontend/src/components/blog/Blog.jsx
+++ corelia_project/frontend/src/components/blog/Blog.jsx
@@ -10,11 +10,18 @@ const FlexContainer = styled.div`
     margin-bottom: ${stylingConstants.sizes.gapFromFooterToEndOfContent};
 `;
 
+const InvisibleFlexItem = styled.div`
+    // Same width as month filter so that posts are centered in middle of page.
+    width: 20%;
+    margin-right: ${stylingConstants.sizes.leftRightMargin};
+`;
+
 const Blog = () => {
     return (
         <FlexContainer>
             <MonthFilters />
             <PostsContainer />
+            <InvisibleFlexItem />
         </FlexContainer>
     );
 };
