d31d211343a5863763a6e67817ce7a0c27641bbd
Mitchell Anton <mitchella0100@gmail.com>
2022-10-07 19:22:40 +1100

    ADd load more butotn (but not functionalti) onto discover composers page.'

--- corelia_project/frontend/src/components/discover-composers/DiscoverComposers.jsx
+++ corelia_project/frontend/src/components/discover-composers/DiscoverComposers.jsx
@@ -1,17 +1,19 @@
 import styled from "styled-components";
 import stylingConstants from "../../utils/styling";
 import FilterBar from "../filter-bar/FilterBar";
-import React, { useEffect } from "react";
+import React from "react";
 import useSearchQuery from "../../hooks/useSearchQuery";
 import useFetchOnPageLoad from "../../hooks/useFetchOnPageLoad";
 import { filterComposersByLetter, getComposers } from "../../api/composers";
 import SearchResultsContainer from "./SearchResultsContainer";
 import useFetchOnParamChange from "../../hooks/useFetchOnParamChange";
+import LoadMoreButton from "../buttons/LoadMoreButton";
 
 // TODO: Layout this page with flex box.
 const FlexContainer = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: center;
     margin-top: ${stylingConstants.sizes.navbarHeight} + 10px;
 `;
 
@@ -35,16 +37,19 @@ const DiscoverComposers = () => {
     } else if (error) {
         composers = <div>{error.message}</div>;
     } else if (data.count && data.count > 0) {
-        composers = <SearchResultsContainer composers={data.results} />;
+        composers = (
+            <>
+                <SearchResultsContainer composers={data.results} />
+                <LoadMoreButton />
+            </>
+        );
     }
 
     return (
-        <>
-            <FlexContainer>
-                <FilterBar initialSearchType={"A-Z"} />
-            </FlexContainer>
+        <FlexContainer>
+            <FilterBar initialSearchType={"A-Z"} />
             {composers}
-        </>
+        </FlexContainer>
     );
 };
 
