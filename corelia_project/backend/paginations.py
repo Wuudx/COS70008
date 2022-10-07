d480d693204357ef09de3098d0dbc5082f68ba7a
PikeyAU <66731279+PikeyAU@users.noreply.github.com>
2022-10-07 19:58:50 +1100

    Default pagination = 5

--- corelia_project/backend/paginations.py
+++ corelia_project/backend/paginations.py
@@ -2,8 +2,8 @@ from email.policy import default
 from rest_framework import pagination
 
 class CustomPagination(pagination.PageNumberPagination):
-    page_size = 2
+    page_size = 5
     page_size_query_param = 'page_size'
     max_page_size = 50
     page_query_param = 'p'
-    default_limit = 5
+    
