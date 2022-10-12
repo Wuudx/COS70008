e0c10b4fea0ddc03b7fcb9634fcea72229baa1f5
PikeyAU <66731279+PikeyAU@users.noreply.github.com>
2022-10-12 21:44:40 +1100

    Forum comments

--- corelia_project/backend/admin.py
+++ corelia_project/backend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Composer, Composition, Instrument, Nationality, ComposerNationality, CompositionInstrument, Publisher
+from .models import Composer, Composition, Instrument, Nationality, ComposerNationality, CompositionInstrument, Publisher, BlogPost, BlogComment, ForumPost, ForumComment, User
 
 admin.site.register(Composer)
 admin.site.register(Composition)
@@ -8,6 +8,12 @@ admin.site.register(Nationality)
 admin.site.register(ComposerNationality)
 admin.site.register(CompositionInstrument)
 admin.site.register(Publisher)
+admin.site.register(BlogPost)
+admin.site.register(BlogComment)
+admin.site.register(ForumPost)
+admin.site.register(ForumComment)
+admin.site.register(User)
+
 
 
 # Register your models here.
