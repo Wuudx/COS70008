127edfd6c489f6c0bfe4838b6a395d3ff1f5a1b4
Mitchell Anton <mitchella0100@gmail.com>
2022-10-12 23:20:39 +1100

    minor forum styling changes

--- corelia_project/frontend/src/components/forum/post/CommentAndShare.jsx
+++ corelia_project/frontend/src/components/forum/post/CommentAndShare.jsx
@@ -8,15 +8,22 @@ import ShareButton from "./ShareButton";
 const StyledLink = styled(Link)`
     color: ${stylingConstants.colours.blue2Percent100};
     text-decoration: none;
+    &:hover {
+        border-bottom: 3px solid ${stylingConstants.colours.blue2Percent30};
+    }
 `;
 
 const CommentAndShare = ({ numComments, postId }) => {
+    const iconStyle = {
+        verticalAlign: "middle",
+    };
+
     return (
         <span>
-            <StyledLink to={`post/${postId}/comments`}>
-                <FaCommentAlt /> {numComments} Comments
+            <StyledLink to={`/forum/post/${postId}/comments`}>
+                <FaCommentAlt style={iconStyle} /> {numComments} Comments
             </StyledLink>{" "}
-            | <ShareButton postId={postId} />
+            | <ShareButton postId={postId} style={iconStyle} />
         </span>
     );
 };
