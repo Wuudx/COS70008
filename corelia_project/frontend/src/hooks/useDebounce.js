6ce7a0ea9c01ec7d2bb585718ec711ef55c3233b
Sebastian <sebastian.cumins@gmail.com>
2022-10-13 22:25:01 +1100

    debounce

---
+++ corelia_project/frontend/src/hooks/useDebounce.js
@@ -0,0 +1,12 @@
+function useDebounce(fn, ms) {
+    let timer;
+    return (_) => {
+        clearTimeout(timer);
+        timer = setTimeout((_) => {
+            timer = null;
+            fn.apply(this, arguments);
+        }, ms);
+    };
+}
+
+export default useDebounce;
