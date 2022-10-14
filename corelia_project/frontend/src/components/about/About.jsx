1c48c3df9a8b6b4c5ad8f0316e2e0a1da11cc4dd
Sebastian <sebastian.cumins@gmail.com>
2022-10-14 15:22:06 +1100

    aboot

--- corelia_project/frontend/src/components/about/About.jsx
+++ corelia_project/frontend/src/components/about/About.jsx
@@ -5,25 +5,46 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
-    margin: 0 auto;
+    justify-content: flex-start;
     padding: 0 1rem 3rem 1rem;
+    width: 70%;
+    height: 100%;
+    align-self: center;
+`;
+
+const H1 = styled.h1`
+    font-family: 'Lato-Bold';
+    font-size: 2em;
+    margin-bottom: 1rem;
+`;
+
+const H2 = styled.h2`
+    font-family: 'Lato-Bold';
+    font-size: 1.5em;
+    margin-bottom: 1rem;
+`;
+
+const P = styled.p`
+    font-family: 'Lato-Regular';
+    font-size: 1em;
+    margin-bottom: 1rem;
+    align-self: flex-start;
 `;
 
 const About = () => {
     return (
         <Container>
-            <h1>Welcome to the Corelia Project!</h1>
-            <p>We are an initiative that spotlights women composers</p>
-            <h2>What are we trying to achieve?</h2>
-            <p>
+            <H1>Welcome to the Corelia Project!</H1>
+            <P>We are an initiative that spotlights women composers</P>
+            <H2>What are we trying to achieve?</H2>
+            <P>
                 Historically, women's voices have been grossly underrepresented
                 in classical music. Despite lot of progress, there is still a
                 very long road ahead. Currently, most core clarinet repertoire
                 lists do not feature a single work written by a woman, despite
                 the countless exceptional pieces that exist!
-            </p>
-            <p>
+            </P>
+            <P>
                 Here at the Corelia Project, we believe that visibility and
                 accessibility are the first steps to driving change. Through the
                 promotion of women composers and their works, we aim to
@@ -31,9 +52,9 @@ const About = () => {
                 to diversify their own repertoire. With the click of a few
                 buttons, clarinetists can discover and be inspired to play new
                 pieces, creating a ripple effect over time.
-            </p>
-            <h2>Corelia Project Founder</h2>
-            <p>
+            </P>
+            <H2>Corelia Project Founder</H2>
+            <P>
                 The Corelia Project was founded by Magdalenna Krstevska, an
                 Australian clarinetist, currently based in London. A highly
                 experienced performer, Magdalenna has performed extensively on
@@ -46,7 +67,7 @@ const About = () => {
                 College of Music, London. She is a Wilkins-Mackerras Award
                 Holder, supported by the Henry Wood Accommodation Trust and a
                 Drake Calleja Trust Scholar.
-            </p>
+            </P>
         </Container>
     );
 };
