de085d4b3047ff2c30f7858d8b838016cd3b2fd1
Mitchell Anton <mitchella0100@gmail.com>
2022-10-14 14:46:07 +1100

    Styling

--- corelia_project/frontend/src/components/forum/YearFilter.jsx
+++ corelia_project/frontend/src/components/forum/YearFilter.jsx
@@ -1,13 +1,34 @@
 import React, { useEffect, useState } from "react";
+import { MdError } from "react-icons/md";
 import { useLocation, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import useSearchQuery from "../../hooks/useSearchQuery";
+import stylingConstants from "../../utils/styling";
+
+const Container = styled.div`
+    width: 100%;
+    div {
+        margin-top: 0.5em;
+    }
+`;
+
+const Input = styled.input`
+    border-radius: ${stylingConstants.sizes.containerBorderRadius};
+    padding: 1em;
+    width: 100%;
+    border: 0px;
+`;
+
+const Span = styled.span`
+    color: red;
+`;
 
 const YearFilter = () => {
     const navigate = useNavigate();
     const { pathname } = useLocation();
     const currentMonthFilter = useSearchQuery("month");
     const [yearFilter, setYearFilter] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
+    const [yearFilterError, setYearFilterError] = useState(false);
 
     // Only allow years from 2022 to 2099.
     function checkYearValid() {
@@ -22,27 +43,38 @@ const YearFilter = () => {
         }
         const isYearValid = checkYearValid();
         if (!isYearValid) {
-            setErrorMessage("Please enter a year between 2022 and 2099.");
+            setYearFilterError(true);
         } else {
-            setErrorMessage("");
+            setYearFilterError(false);
             navigate(
                 `${pathname}?month=${currentMonthFilter}&year=${yearFilter}`
             );
         }
     }, [yearFilter]);
 
+    let yearFilterErrorElement = "";
+    if (yearFilterError) {
+        yearFilterErrorElement = (
+            <div>
+                <MdError color="red" style={{ verticalAlign: "middle" }} />
+                <Span>Please enter a year between 2022 and 2099</Span>
+            </div>
+        );
+    }
+
     return (
-        <div>
-            <input
+        <Container>
+            <Input
                 type="number"
                 min="2022"
                 max="2099"
                 step="1"
+                placeholder="year..."
                 value={yearFilter}
                 onChange={(e) => setYearFilter(e.target.value)}
             />
-            <span>{errorMessage}</span>
-        </div>
+            {yearFilterErrorElement}
+        </Container>
     );
 };
 
