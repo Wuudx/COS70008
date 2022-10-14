8bd98bdc8b05ff71b034b764ba4ad1e600b3dd13
Sebastian <sebastian.cumins@gmail.com>
2022-10-14 14:42:29 +1100

    blog filtering works

--- corelia_project/frontend/src/api/blogs.js
+++ corelia_project/frontend/src/api/blogs.js
@@ -13,3 +13,17 @@ export async function getBlogPost(id) {
     }
     return response.json();
 }
+
+export async function getBlogPostsByMonth(month) {
+    if (!month) {
+        return getBlogPosts();
+    }
+
+    const url = `http://localhost:8000/api/blogs/${month}/posts?limit=200`; // limit hack :(
+
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+    return response.json();
+}
